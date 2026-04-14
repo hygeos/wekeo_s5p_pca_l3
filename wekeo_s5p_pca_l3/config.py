94edde85d9d3b7128602d9cec75c69541a16baba
Joackim Orcière <joackim.orciere@hygeos.com>
2026-04-14 14:24:11 +0200

    Improved config

--- wekeo_s5p_pca_l3/config.py
+++ wekeo_s5p_pca_l3/config.py
@@ -1,13 +1,15 @@
 from wekeo_s5p_pca_l3.hygeos_core import env
 output_dir = env.getdir("OUTPUT_DIR")
-frp_download_dir = env.getdir("DIR_ANCILLARY") / "SLSTR_FRP"
+dir_ancillary = env.getdir("DIR_ANCILLARY")
 
-if not frp_download_dir.exists():
-    raise FileNotFoundError(f"FRP download directory {frp_download_dir} does not exist. Please create it or check your environment configuration.")
+if not dir_ancillary.exists():
+    raise FileNotFoundError(f"Ancillary directory {dir_ancillary} does not exist. Please create it or check your environment configuration.")
 
 if not output_dir.exists():
     raise FileNotFoundError(f"Output directory {output_dir} does not exist. Please create it or check your environment configuration.")
-
+    
+frp_download_dir = dir_ancillary / "SLSTR_FRP"
+frp_download_dir.mkdir(parents=False, exist_ok=True)
 
 gridded_s5p_pca_dir = output_dir / "gridded_s5p_pca_l3"
 gridded_s5p_pca_dir.mkdir(parents=False, exist_ok=True)
